a44692fe12ace730bd04bdcfff5ceb9801770a69
yasmirnov <iaroslav.irnovsm@gmail.com>
2022-12-11 17:51:39 +0300

    utils.py update

---
+++ utils.py
@@ -0,0 +1,39 @@
+import json
+
+CANDIDATES_FILE = "candidates.json"
+
+
+def load_candidates() -> list[dict]:
+    """
+    загружает данные из файла
+    """
+    with open(CANDIDATES_FILE, "r") as file:
+        return json.load(file)
+
+
+def get_candidates_all():
+    """
+    получает всех кандидатов
+    """
+    return load_candidates()
+
+
+def get_by_pk(pk):
+    """
+    возвращает кандидата по pk
+    """
+    for candidate in load_candidates():
+        if int(pk) == candidate["pk"]:
+            return candidate
+    return None
+
+
+def get_by_skill(skill_name) -> list:
+    """
+    возвращает кандидатов по навыку
+    """
+    candidates = []
+    for data_candidate in load_candidates():
+        if skill_name.lower() in data_candidate["skills"].lower().split(', '):
+            candidates.append(data_candidate)
+    return candidates
